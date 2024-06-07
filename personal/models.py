5212497f955804d87c6e91918acc75da9cd9907a
Bezopasn1k <bulgakov_59@inbox.ru>
2024-06-07 15:37:31 +0700

    hh

--- personal/models.py
+++ personal/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
 class Personal(models.Model):
-    name = models.CharField(max_length=150, unique=False, blank=False, null=False, verbose_name='Название')
+    id_personal = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=255, verbose_name = 'Фамилия')
+    first_name = models.CharField(max_length=255, verbose_name = 'Имя')
+    second_name = models.CharField(max_length=255,blank=True, null=True, verbose_name = 'Отчество')
+    t_n = models.CharField(max_length=8, verbose_name='Табельный номер сотрудника')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    uch = models.CharField(max_length=10,blank=False,null=False,  verbose_name = 'Участок работы')
+    rank = models.CharField(max_length=50,blank=False,null=False, verbose_name = 'Должность')
+    sex = models.CharField(max_length=10,blank=False, null=False, verbose_name = 'Пол')
+    t_tel = models.CharField(max_length=50, blank=False, null=False, verbose_name = 'Номер рабочего телефона')
+    r_tel = models.CharField(max_length=50,blank=True, null=True, verbose_name = 'Номер личного телефона')
+    zdo = models.BooleanField(blank=False, verbose_name = 'Может ли сотрудник выполнять тяжелую работу')
+
+    def last_name_with_initials(self):
+        initials = f"{self.first_name[0]}. {self.second_name[0]}." if self.second_name else f"{self.first_name[0]}."
+        return f"{self.last_name} {initials}"
+
   
     class Meta:
         db_table = 'Personal'
