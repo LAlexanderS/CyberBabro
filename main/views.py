6b4e81eed02ae84fcffe43f87d539bb206071fc5
LAlexanderS <likov.shura2003@mail.ru>
2024-06-14 18:03:39 +0700

    db forms applications, scriptModelTest

--- main/views.py
+++ main/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import render
+from .models import Personalapplication
+from model import routes, schedule
+from metro.models import Stationtime, Transfertime
 
 def index(request):
-  return render(request, 'main/index.html')
+    routes = routes.Routes()
+    routes.CalcDistance(Stationtime.id_st1, Stationtime.id_st2)
+    routes.CreateShortestPath(Transfertime.id1,Transfertime.id2)
+
+    personal_applications = Personalapplication.objects.select_related('person', 'application').all()
+    context = {
+        'personal_applications': personal_applications
+    }
+    return render(request, 'main/index.html', context)
